4ea6ed2773d11431fb6b4e339cf3f40493f04667
adam <adamwickett@hotmail.com>
2018-12-08 12:41:53 +0000

    hover

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ const loadFile = path => {
 const movies = require('./data/movies_with_ids.json');
 
 const generateMovieSummaryHtml = movie => {
-  return `<div class ='movie'>
-    <div class='image-frame'>
-      <a href='/movies/${movie.id}'><img src='${movie.poster_url}'></a>
-    </div>  
-    <h2>${movie.title}</h2>
+  return `<div class='movie'>
+    <a class='movie-link' href='/movies/${movie.id}'>
+      <div class='image-frame'>
+        <img src='${movie.poster_url}'>
+        <h2>${movie.title}</h2>
+      </div>
+    </a>
   </div>`
 }
 
