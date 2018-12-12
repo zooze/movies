f08cdf97dcc876e0395d58dc0222b3a41c1ac856
Chris Wickett <chris@chriswickett.com>
2018-12-12 17:11:14 +0000

    Indentation

--- examples/build_html_with_map.js
+++ examples/build_html_with_map.js
@@ -13,3 +13,14 @@ let htmlArray = genre.map(genre => {
 }
 let genresHtml = generateListHtml(genres);
 console.log(genresHtml);
+
+
+// Correct indentation below. Also, just return htmlArray.join('') rather than creating a variable for it. It's quicker.
+const generateListHtml = genre => {
+  let htmlArray = genre.map(genre => {
+    return `<li>${genre}</li>`;
+  });
+  htmlArray.unshift('<ul>'); // Add opening tag to the beginning
+  htmlArray.push('</ul>'); // Add closing tag to the end
+  return htmlArray.join(''); // Join array into a string
+}
