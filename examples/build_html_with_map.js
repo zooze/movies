d59196813e7f7fa467bc094ace41c7554ef1f25e
adam <adamwickett@hotmail.com>
2018-12-12 17:40:36 +0000

    refactor

--- examples/build_html_with_map.js
+++ examples/build_html_with_map.js
@@ -3,24 +3,13 @@ let genres = require('../data/genres.json');
 
 // Build array of <li> elements
 const generateListHtml = genre =>{
-let htmlArray = genre.map(genre => {
-  return `<li>${genre}</li>`;
+  let liElementArray = genre.map(genre => {
+    return `<li>${genre}</li>`;
   });
-  htmlArray.unshift('<ul>'); // Add opening tag to the beginning
-  htmlArray.push('</ul>'); // Add closing tag to the end
-  let html = htmlArray.join(''); // Join array into a string
-  return html;
+  return [].concat('<ul>', liElementArray, '</ul>').join('');
+
 }
 let genresHtml = generateListHtml(genres);
 console.log(genresHtml);
 
 
-// Correct indentation below. Also, just return htmlArray.join('') rather than creating a variable for it. It's quicker.
-const generateListHtml = genre => {
-  let htmlArray = genre.map(genre => {
-    return `<li>${genre}</li>`;
-  });
-  htmlArray.unshift('<ul>'); // Add opening tag to the beginning
-  htmlArray.push('</ul>'); // Add closing tag to the end
-  return htmlArray.join(''); // Join array into a string
-}
