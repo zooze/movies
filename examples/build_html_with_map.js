c9b1a4f33dbf6f2c9fbbd611f4472b6e73805647
Chris Wickett <chris@chriswickett.com>
2018-12-12 17:56:21 +0000

    New method

--- examples/build_html_with_map.js
+++ examples/build_html_with_map.js
@@ -2,25 +2,36 @@
 let genres = require('../data/genres.json');
 
 // Build array of <li> elements
-const generateListHtml = genre =>{
-let htmlArray = genre.map(genre => {
-  return `<li>${genre}</li>`;
-  });
-  htmlArray.unshift('<ul>'); // Add opening tag to the beginning
-  htmlArray.push('</ul>'); // Add closing tag to the end
-  let html = htmlArray.join(''); // Join array into a string
-  return html;
-}
-let genresHtml = generateListHtml(genres);
-console.log(genresHtml);
+// const generateListHtml = genre =>{
+// let htmlArray = genre.map(genre => {
+//   return `<li>${genre}</li>`;
+//   });
+//   htmlArray.unshift('<ul>'); // Add opening tag to the beginning
+//   htmlArray.push('</ul>'); // Add closing tag to the end
+//   let html = htmlArray.join(''); // Join array into a string
+//   return html;
+// }
+// let genresHtml = generateListHtml(genres);
+// console.log(genresHtml);
 
 
 // Correct indentation below. Also, just return htmlArray.join('') rather than creating a variable for it. It's quicker.
-const generateListHtml = genre => {
-  let htmlArray = genre.map(genre => {
-    return `<li>${genre}</li>`;
-  });
-  htmlArray.unshift('<ul>'); // Add opening tag to the beginning
-  htmlArray.push('</ul>'); // Add closing tag to the end
-  return htmlArray.join(''); // Join array into a string
+// const generateListHtml = genre => {
+//   let htmlArray = genre.map(genre => {
+//     return `<li>${genre}</li>`;
+//   });
+//   htmlArray.unshift('<ul>'); // Add opening tag to the beginning
+//   htmlArray.push('</ul>'); // Add closing tag to the end
+//   return htmlArray.join(''); // Join array into a string
+// }
+
+
+// My absolute favourite way
+const generateListHtml = arr => {
+  let liElements = arr.map(item => `<li>${item}</li>`).join('');
+  return `<ul>${liElements}</ul>`;
 }
+
+
+console.log(generateListHtml(genres));
+
